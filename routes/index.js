4707a5c155b73947501a3fc2ce864a444fb84443
tayutaedomo <tayutaedomo@gmail.com>
2017-07-27 03:12:34 +0900

    Add project base files

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Webpack v3 Sandbox' });
+});
+
+module.exports = router;
